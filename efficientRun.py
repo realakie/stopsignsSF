d6b066931bc5ba0114b9eae148324b5525bc715b
realakie <hashimoto.akie@gmail.com>
2022-09-06 13:07:23 -0700

    Initial commit

--- efficientRun.py
+++ efficientRun.py
@@ -1,16 +1,15 @@
 import main
-from main import *
 
-#data from https://data.sfgov.org/Transportation/Stop-Signs/4542-gpa3
-dataStopSign = main.cleanup('Stopsigns.csv') #API didn't include Neighborhoods column
-start = [35,'Q']
-end = [40,'I']
+# data from https://data.sfgov.org/Transportation/Stop-Signs/4542-gpa3
+dataStopSign = main.cleanup('Stopsigns.csv')  # API didn't include Neighborhoods column
+start = [35, 'Q']
+end = [40, 'I']
 if (end[0] - start[0]) == 0 or (ord(end[1]) - ord(start[1])) == 0:
     print("Go Straight")
 result = main.minstops(dataStopSign, start, end, 0, 0, 'Start')
 print("Minimum Number of Stop signs encountered: ", result[0])
 print(result[1])
 
-#thisloc = [[35, 'Q']]
-#thisloc.append([34,'R'])
-#print(thisloc)
+# thisloc = [[35, 'Q']]
+# thisloc.append([34,'R'])
+# print(thisloc)
--- main.py
+++ main.py
@@ -8,113 +8,110 @@ import pandas as pd
 import re
 
 
-
 def cleanup(filename):
-    df = pd.read_csv(filename,header = 1) # Filter the top row
-    df = df[['STREET','X_STREET','ST_FACING','Neighborhoods']] #filtering by the column names
-    df['Neighborhoods']=df['Neighborhoods'].fillna(0).astype(int) #filtering by the neighborhood
-    sunsetneighborhood = [39,40,46,109]
-    filter = df.Neighborhoods.isin(sunsetneighborhood)
-    df = df[filter]
-    df = df.reset_index(drop = True)
-    #print(df)
+    df = pd.read_csv(filename, header=1)  # Filter the top row
+    df = df[['STREET', 'X_STREET', 'ST_FACING', 'Neighborhoods']]  # filtering by the column names
+    df['Neighborhoods'] = df['Neighborhoods'].fillna(0).astype(int)  # filtering by the neighborhood
+    sunsetneighborhood = [39, 40, 46, 109]
+    filterval = df.Neighborhoods.isin(sunsetneighborhood)
+    df = df[filterval]
+    df = df.reset_index(drop=True)
+    # print(df)
 
-    #Clear the names of the street to numbers and x_street to ABCs
-    for x in df.index: #for each row
-        streetFirst = df.iat[x,0]
-        streetSecond = df.iat[x,1]
+    # Clear the names of the street to numbers and x_street to ABCs
+    for x in df.index:  # for each row
+        streetFirst = df.iat[x, 0]
+        streetSecond = df.iat[x, 1]
         numStreet = streetFirst
         abcStreet = streetSecond
 
         if streetFirst[0].isdigit():
-            numStreet = int(re.findall('\d+',streetFirst)[0])
-            abcStreet = streetSecond[0] #Get the first letter of the Aphabet
+            numStreet = int(re.findall('\d+', streetFirst)[0])
+            abcStreet = streetSecond[0]  # Get the first letter of the Aphabet
         elif streetSecond[0].isdigit():
-            #Flip the two column values
-            numStreet = int(re.findall('\d+',streetSecond)[0])
+            # Flip the two column values
+            numStreet = int(re.findall('\d+', streetSecond)[0])
             abcStreet = streetFirst[0]
         else:
-            #neigher street has numbers remove the row
+            # neigher street has numbers remove the row
             numStreet = 'Remove'
             abcStreet = 'Remove'
 
-        df.loc[x,'STREET'] = numStreet
-        df.loc[x,'X_STREET'] = abcStreet
-    df.drop_duplicates(inplace = True) #clean up duplicates
-    df = df[df['STREET'] !='Remove']
-    df = df.reset_index(drop = True)
-    #print(df)
+        df.loc[x, 'STREET'] = numStreet
+        df.loc[x, 'X_STREET'] = abcStreet
+    df.drop_duplicates(inplace=True)  # clean up duplicates
+    df = df[df['STREET'] != 'Remove']
+    df = df.reset_index(drop=True)
+    # print(df)
 
-    #Combine All the stopsign directions into one row by grouping
+    # Combine All the stopsign directions into one row by grouping
     df.ST_FACING = df.ST_FACING.map(lambda x: x.rstrip('B'))
-    df.ST_FACING = df.groupby(['STREET','X_STREET'])['ST_FACING'].transform(lambda x: ''.join(x))
-    df.drop_duplicates(inplace = True)
-    df = df.reset_index(drop = True)
+    df.ST_FACING = df.groupby(['STREET', 'X_STREET'])['ST_FACING'].transform(lambda x: ''.join(x))
+    df.drop_duplicates(inplace=True)
+    df = df.reset_index(drop=True)
 
     return df
-    #print(df)
+    # print(df)
 
 
-#Calculate minimum stops required from pointA to pointB
+# Calculate minimum stops required from pointA to pointB
 
-def minstops(df, start, end, i, j ,direction):
-    disH = end[0] - start [0]
-    disV = ord(end[1])-ord(start[1])
+def minstops(df, start, end, i, j, direction):
+    disH = end[0] - start[0]
+    disV = ord(end[1]) - ord(start[1])
 
-    if abs(i) > abs(disH) or abs(j) > abs(disV): #out of range
-        #print("Out of Range: ",i," ",j)
-        return 1000, [100,100]
+    if abs(i) > abs(disH) or abs(j) > abs(disV):  # out of range
+        # print("Out of Range: ",i," ",j)
+        return 1000, [100, 100]
 
-    #set the general Move direction based on the start and end points
+    # set the general Move direction based on the start and end points
     going = ''
     move = []
     if disH < 0 and disV > 0:
         going = 'ES'
-        move = [-1,1]
+        move = [-1, 1]
     elif disH < 0 and disV < 0:
         going = 'EN'
-        move = [-1,-1]
+        move = [-1, -1]
     elif disH > 0 and disV > 0:
         going = 'WS'
-        move = [1,1]
+        move = [1, 1]
     elif disH > 0 and disV < 0:
         going = 'WN'
-        move = [1,-1]
+        move = [1, -1]
 
-    #Current Location
-    location = start[0] + move[0]*i, chr(ord(start[1]) + move[1]*j)
-    #print("Current Location: ",location)
+    # Current Location
+    location = start[0] + move[0] * i, chr(ord(start[1]) + move[1] * j)
+    # print("Current Location: ",location)
 
-    #Is there a stop sign at this location?
+    # Is there a stop sign at this location?
     try:
-        stopCorner = df.loc[(df.STREET == location[0]) & (df.X_STREET == location[1]),'ST_FACING'].values[0]
+        stopCorner = df.loc[(df.STREET == location[0]) & (df.X_STREET == location[1]), 'ST_FACING'].values[0]
     except IndexError:
         stopCorner = 'none'
 
     numStop = 0
-    #Is there a stopsign at the direction it is coming from?
+    # Is there a stopsign at the direction it is coming from?
     if ((direction == 'H') & (going[0] in stopCorner)) | ((direction == 'V') & (going[1] in stopCorner)):
         numStop = 1
 
-    #reached the destination
+    # reached the destination
     if abs(i) == abs(disH) and abs(j) == abs(disV):
-        #print("at the end: ",end)
+        # print("at the end: ",end)
         return numStop, end
-    #This corner is nott all way stop - avoid this intersection
+    # This corner is nott all way stop - avoid this intersection
     elif numStop == 1 and len(stopCorner) < 4:
-        #print("avoid this intersection: ",location)
+        # print("avoid this intersection: ",location)
         return 1000, location
 
-    #Keep going down the recursion
+    # Keep going down the recursion
     else:
-        #which direction returns less number of stops?
-        horizontal = minstops(df, start, end, i+1, j, 'H')
-        vertical = minstops(df, start, end, i, j+1, 'V')
-        #Passing the pathway in the second parameter
+        # which direction returns less number of stops?
+        horizontal = minstops(df, start, end, i + 1, j, 'H')
+        vertical = minstops(df, start, end, i, j + 1, 'V')
+        # Passing the pathway in the second parameter
 
         if horizontal[0] < vertical[0]:
-            return numStop + horizontal[0], [location,horizontal[1]]
+            return numStop + horizontal[0], [location, horizontal[1]]
         else:
-            return numStop + vertical[0], [location,vertical[1]]
-
-
+            return numStop + vertical[0], [location, vertical[1]]
